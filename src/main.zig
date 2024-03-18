0419ce0290b6e30e62cc34f9e1de03514381512d
mahdifrmz <faramarzpour98@gmail.com>
2024-03-18 22:06:12 +0330

    arg parsing

--- src/main.zig
+++ src/main.zig
@@ -52,16 +52,8 @@ fn readHeader(idx: TagType, strm: anytype) !Header {
 
 const SYSTEM_ENDIANNESS = builtin.target.cpu.arch.endian();
 
-fn toProtoclEndianness(comptime T: type, num: T) T {
-    if (SYSTEM_ENDIANNESS == std.builtin.Endian.Little) {
-        return num;
-    } else {
-        return @byteSwap(num);
-    }
-}
-
 fn writeMessage(mes: Message, strm: anytype) !void {
-    const idx = toProtoclEndianness(TagType, @intFromEnum(mes.header));
+    const idx = std.mem.nativeToLittle(TagType, @intFromEnum(mes.header));
     try strm.writeAll(std.mem.asBytes(&idx));
     try writeHeader(&mes.header, strm);
 }
@@ -74,12 +66,207 @@ fn readMessage(strm: anytype) !Message {
     };
 }
 
+fn ArrayListFromIterator(
+    comptime T: type,
+    iter: anytype,
+    allocator: std.mem.Allocator,
+) !std.ArrayList(T) {
+    var list = std.ArrayList(T).init(allocator);
+    var iter_mut = iter;
+    while (iter_mut.next()) |n| {
+        try list.append(n);
+    }
+    return list;
+}
+
+const ExecMode = enum {
+    Client,
+    Server,
+};
+
+const Arguments = struct {
+    mode: ?ExecMode,
+    address: ?[]const u8,
+    port: ?[]const u8,
+    help: bool,
+};
+
+const ConfigError = error{
+    MissingOption,
+    InvalidPort,
+    UnknownFlag,
+};
+
+const ArgParser = struct {
+    const Self = @This();
+
+    current: ?[:0]const u8,
+    iterator: std.process.ArgIterator,
+    arguments: Arguments,
+    fault: ?[]const u8,
+
+    fn init(iterator: std.process.ArgIterator) Self {
+        return .{
+            .iterator = iterator,
+            .arguments = .{
+                .port = undefined,
+                .mode = undefined,
+                .address = undefined,
+                .help = false,
+            },
+            .fault = undefined,
+            .current = undefined,
+        };
+    }
+
+    fn peek(self: *Self) ?[:0]const u8 {
+        if (self.current) |_| {} else {
+            self.current = self.iterator.next();
+        }
+        return self.current;
+    }
+
+    fn next(self: *Self) ?[:0]const u8 {
+        if (self.current) |token| {
+            self.current = undefined;
+            return token;
+        }
+        return self.iterator.next();
+    }
+
+    fn is_flag(self: *Self) bool {
+        const token = self.peek();
+        if (token) |tkn| {
+            return tkn.len == 2 and tkn[0] == '-';
+        }
+        return false;
+    }
+
+    fn expect(self: *Self, name: []const u8) ![:0]const u8 {
+        if (self.next()) |token| {
+            return token;
+        } else {
+            self.fault = name;
+            return ConfigError.MissingOption;
+        }
+    }
+
+    fn nextFlag(self: *Self) !void {
+        const token = self.next() orelse unreachable;
+        const flag = token[1];
+        switch (flag) {
+            'c' => {
+                self.arguments.mode = ExecMode.Client;
+            },
+            'h' => {
+                self.arguments.help = true;
+            },
+            's' => {
+                self.arguments.mode = ExecMode.Server;
+            },
+            'a' => {
+                self.arguments.address = try self.expect("address");
+            },
+            'p' => {
+                self.arguments.port = try self.expect("port");
+            },
+            else => {
+                self.fault = token;
+                return ConfigError.UnknownFlag;
+            },
+        }
+    }
+
+    fn nextPositional(self: *Self, name: []const u8) ![:0]const u8 {
+        while (self.is_flag()) {
+            try self.nextFlag();
+        }
+        if (self.next()) |value| {
+            return value;
+        } else {
+            self.fault = name;
+            return ConfigError.MissingOption;
+        }
+    }
+
+    fn parseAllFlags(self: *Self) !void {
+        while (self.is_flag()) {
+            try self.nextFlag();
+        }
+    }
+
+    fn parse(self: *Self) !Arguments {
+        try self.parseAllFlags();
+        return self.arguments;
+    }
+};
+
+const DEFAULT_PORT = 7070;
+
+const Config = struct {
+    const Self = @This();
+
+    address: []const u8,
+    port: u16,
+    is_server: bool,
+
+    fn parsePort(self: *Self, args: Arguments) !void {
+        if (args.port) |p| {
+            self.port = switch (std.zig.parseNumberLiteral(p)) {
+                .failure, .float, .big_int => return ConfigError.InvalidPort,
+                .int => |num| blk: {
+                    if (num < 1 or num > 0xffff) {
+                        return ConfigError.InvalidPort;
+                    } else {
+                        break :blk @intCast(num);
+                    }
+                },
+            };
+        }
+    }
+
+    fn parseAddress(self: *Self, args: Arguments) !void {
+        if (args.address) |addr| {
+            if (std.net.isValidHostName(addr)) {
+                self.address = addr;
+            }
+        }
+    }
+
+    fn parseCLI(self: *Self, args: Arguments) !void {
+        try self.parsePort(args);
+        try self.parseAddress(args);
+        if (args.mode) |m| {
+            self.is_server = m == ExecMode.Server;
+        }
+    }
+
+    fn init() Self {
+        return .{
+            .is_server = false,
+            .address = "0.0.0.0",
+            .port = DEFAULT_PORT,
+        };
+    }
+};
+
+fn showHelp() noreturn {
+    std.io.getStdOut().writer().writeAll("HELP\n") catch {};
+    std.process.exit(0);
+}
+
+fn loadConfig() !Config {
+    var parser = ArgParser.init(std.process.args());
+    const args = parser.parse() catch return showHelp();
+    if (args.help) {
+        return showHelp();
+    }
+    var conf = Config.init();
+    try conf.parseCLI(args);
+    return conf;
+}
+
 pub fn main() !void {
-    var buffer = [_]u8{0} ** 32;
-    var bufferStream = std.io.fixedBufferStream(buffer[0..]);
-    var strm = bufferStream.writer();
-    const mes = Message{
-        .header = Header{ .Ping = .{ .num = 4 } },
-    };
-    try writeMessage(mes, strm);
+    const conf = try loadConfig();
+    _ = conf;
 }
