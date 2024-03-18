0a04392a72d98695473a0e41de51b58b1a8e7d11
mahdifrmz <faramarzpour98@gmail.com>
2024-03-18 16:19:21 +0330

    (WIP) read header

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,17 @@
+const builtin = @import("builtin");
 const std = @import("std");
-const expect = @import("std").testing.expect;
+const expect = std.testing.expect;
 
-const Header = union(enum) {
+const TagType = u16;
+
+const Header = union(enum(TagType)) {
+    None: packed struct {},
     Ping: packed struct { num: u32 },
     PingReply: packed struct { num: u32 },
 };
 
 const Message = struct {
     header: Header,
-    body: ?[]u8,
 };
 
 fn writeHeader(header: *const Header, strm: anytype) !void {
@@ -29,13 +32,46 @@ fn writeHeader(header: *const Header, strm: anytype) !void {
     unreachable;
 }
 
+fn readHeader(idx: TagType, strm: anytype) !Header {
+    const fields = @typeInfo(Header).Union.fields;
+    const options = @typeInfo(@typeInfo(Header).Union.tag_type orelse unreachable).Enum.fields;
+
+    inline for (fields) |f| {
+        inline for (options) |o| {
+            if (std.mem.eql(u8, o.name, f.name)) {
+                if (o.value == idx) {
+                    var header: Header = .{ .None = {} };
+                    return try strm.readAll(std.mem.asBytes(&@field(header.*, f.name)));
+                }
+            }
+        }
+    }
+
+    unreachable;
+}
+
+const SYSTEM_ENDIANNESS = builtin.target.cpu.arch.endian();
+
+fn toProtoclEndianness(comptime T: type, num: T) T {
+    if (SYSTEM_ENDIANNESS == std.builtin.Endian.Little) {
+        return num;
+    } else {
+        return @byteSwap(num);
+    }
+}
+
 fn writeMessage(mes: Message, strm: anytype) !void {
-    const idx = @intFromEnum(mes.header);
+    const idx = toProtoclEndianness(TagType, @intFromEnum(mes.header));
     try strm.writeAll(std.mem.asBytes(&idx));
     try writeHeader(&mes.header, strm);
-    if (mes.body) |body| {
-        try strm.writeAll(body);
-    }
+}
+
+fn readMessage(strm: anytype) !Message {
+    const idx = try strm.readIntLittle(TagType);
+    const header = try readHeader(idx, strm);
+    return Message{
+        .header = header,
+    };
 }
 
 pub fn main() !void {
@@ -44,11 +80,6 @@ pub fn main() !void {
     var strm = bufferStream.writer();
     const mes = Message{
         .header = Header{ .Ping = .{ .num = 4 } },
-        .body = undefined,
     };
     try writeMessage(mes, strm);
-    // for (buffer) |e| {
-    //     std.debug.print("-> {d}\n", .{e});
-    // }
-    std.debug.print("S = {d}", .{@sizeOf(?u64)});
 }
