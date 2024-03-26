89b9e079ff1ac4185ee16a34e4a0aead5062038a
mahdifrmz <faramarzpour98@gmail.com>
2024-03-27 01:37:29 +0330

    refactor repl

--- src/repl.zig
+++ src/repl.zig
@@ -15,11 +15,13 @@ const CliError = error{
 };
 
 const Self = @This();
+const CommandMap = std.StringHashMap(*const fn (self: *Self) anyerror!void);
 
 config: Config,
 client: Client(net.Stream),
 params: std.ArrayList([]const u8),
 is_exiting: bool,
+command_table: CommandMap,
 
 fn split(self: *Self, line: []const u8) !void {
     errdefer self.params.clearAndFree();
@@ -45,61 +47,68 @@ fn makePrompt(self: *Self) ![]const u8 {
     return prompt;
 }
 
-fn exec(self: *Self) !bool {
+fn exec(self: *Self) !void {
     if (self.params.items.len == 0)
-        return false;
+        return;
     const command = try self.next();
-    if (std.mem.eql(u8, command, "cd")) {
-        const path = try self.next();
-        try self.client.setCwd(path);
-    } else if (std.mem.eql(u8, command, "pwd")) {
-        const path = try self.client.getCwdAlloc(self.config.allocator);
-        defer self.config.allocator.free(path);
-        log.println(path);
-    } else if (std.mem.eql(u8, command, "quit")) {
-        return true;
-    } else if (std.mem.eql(u8, command, "ping")) {
-        try self.client.ping();
-        log.println("the server is up");
-    } else if (std.mem.eql(u8, command, "cat")) {
-        _ = try self.client.getFile(try self.next(), std.io.getStdOut().writer());
-    } else if (std.mem.eql(u8, command, "get")) {
-        const remote_path = try self.next();
-        const local_path = try self.next();
-        const local_file = std.fs.cwd().createFile(local_path, .{}) catch {
-            log.errPrintFmt("failed to open local file: {s}\n", .{local_path});
-            return false;
-        };
-        defer local_file.close();
-        _ = try self.client.getFile(remote_path, local_file.writer());
-    } else if (std.mem.eql(u8, command, "put")) {
-        const remote_path = try self.next();
-        const local_path = try self.next();
-        const local_file = std.fs.cwd().openFile(local_path, .{}) catch {
-            log.errPrintFmt("failed to open local file: {s}\n", .{local_path});
-            return false;
-        };
-        const local_file_stat = try local_file.stat();
-        defer local_file.close();
-        _ = try self.client.putFile(remote_path, local_file.reader(), local_file_stat.size);
-    } else if (std.mem.eql(u8, command, "ls")) {
-        try self.client.getEntries(self.next() catch ".");
-        var buf = [_]u8{0} ** 256;
-        var entry = Entry{
-            .name_buffer = buf[0..],
-            .name = undefined,
-            .is_dir = undefined,
-        };
-        while (try self.client.readEntry(&entry)) {
-            log.println(entry.name);
-        }
-    } else {
-        return error.UnknownCommand;
-    }
-    return false;
+    const cb = self.command_table.get(command) orelse return error.UnknownCommand;
+    try cb(self);
 }
 
-fn runloop(self: *Self) !bool {
+fn command_cd(self: *Self) !void {
+    const path = try self.next();
+    try self.client.setCwd(path);
+}
+fn command_pwd(self: *Self) !void {
+    const path = try self.client.getCwdAlloc(self.config.allocator);
+    defer self.config.allocator.free(path);
+    log.println(path);
+}
+fn command_quit(self: *Self) !void {
+    self.is_exiting = true;
+}
+fn command_ping(self: *Self) !void {
+    try self.client.ping();
+    log.println("the server is up");
+}
+fn command_cat(self: *Self) !void {
+    _ = try self.client.getFile(try self.next(), std.io.getStdOut().writer());
+}
+fn command_get(self: *Self) !void {
+    const remote_path = try self.next();
+    const local_path = try self.next();
+    const local_file = std.fs.cwd().createFile(local_path, .{}) catch {
+        log.errPrintFmt("failed to open local file: {s}\n", .{local_path});
+        return;
+    };
+    defer local_file.close();
+    _ = try self.client.getFile(remote_path, local_file.writer());
+}
+fn command_put(self: *Self) !void {
+    const remote_path = try self.next();
+    const local_path = try self.next();
+    const local_file = std.fs.cwd().openFile(local_path, .{}) catch {
+        log.errPrintFmt("failed to open local file: {s}\n", .{local_path});
+        return;
+    };
+    const local_file_stat = try local_file.stat();
+    defer local_file.close();
+    _ = try self.client.putFile(remote_path, local_file.reader(), local_file_stat.size);
+}
+fn command_ls(self: *Self) !void {
+    try self.client.getEntries(self.next() catch ".");
+    var buf = [_]u8{0} ** 256;
+    var entry = Entry{
+        .name_buffer = buf[0..],
+        .name = undefined,
+        .is_dir = undefined,
+    };
+    while (try self.client.readEntry(&entry)) {
+        log.println(entry.name);
+    }
+}
+
+fn runloop(self: *Self) !void {
     // print prompt
     const prompt = try self.makePrompt();
     defer self.config.allocator.free(prompt);
@@ -108,20 +117,23 @@ fn runloop(self: *Self) !bool {
     var line_buffer = [_]u8{0} ** 0x1000;
     const len = try std.io.getStdIn().read(line_buffer[0..]);
     // exit on ^D
-    if (len == 0)
-        return true;
+    if (len == 0) {
+        self.is_exiting = true;
+        return;
+    }
     // execute
     try self.split(line_buffer[0..len]);
     defer self.params.clearAndFree();
-    return self.exec();
+    try self.exec();
 }
 
 pub fn mainloop(self: *Self, config: Config) !void {
     self.client = Client(net.Stream).init();
+    try self.install_commands();
     const stream = try net.tcpConnectToHost(config.allocator, config.address, config.port);
     try self.client.connect(stream);
-    while (true) {
-        const will_exit = self.runloop() catch |err| {
+    while (!self.is_exiting) {
+        self.runloop() catch |err| {
             const error_text = switch (err) {
                 error.UnknownCommand,
                 error.NotEnoughArgs,
@@ -136,12 +148,20 @@ pub fn mainloop(self: *Self, config: Config) !void {
             log.printFmt("Error: {s}\n", .{error_text});
             continue;
         };
-        if (will_exit) {
-            break;
-        }
     }
     try self.client.close();
     stream.close();
+}
+
+fn install_commands(self: *Self) !void {
+    try self.command_table.put("put", command_put);
+    try self.command_table.put("get", command_get);
+    try self.command_table.put("ls", command_ls);
+    try self.command_table.put("quit", command_quit);
+    try self.command_table.put("ping", command_ping);
+    try self.command_table.put("cat", command_cat);
+    try self.command_table.put("pwd", command_pwd);
+    try self.command_table.put("cd", command_cd);
 }
 
 pub fn init(config: Config) Self {
@@ -150,5 +170,6 @@ pub fn init(config: Config) Self {
         .is_exiting = false,
         .client = undefined,
         .params = std.ArrayList([]const u8).init(config.allocator),
+        .command_table = CommandMap.init(config.allocator),
     };
 }
