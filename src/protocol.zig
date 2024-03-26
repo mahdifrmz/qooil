13a32e1aa3c2aea0b3d399ae0009ee192ecb020a
mahdifrmz <faramarzpour98@gmail.com>
2024-03-26 16:43:32 +0330

    documenting protocol

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,5 +1,115 @@
 const std = @import("std");
+/// **The Protocol**
+///
+/// The communication between the server and client starts by the client opening
+/// a TCP connection to the server and terminates by either of the peers closing
+/// the connection or the client sending the `quit` message.
+/// Multiple messages can be sent in a single TCP connection. For each message
+/// that the client sends the server MUST send back the `Error` message, or the
+/// corresponding message types as a response, with that being just a simple `OK`
+/// message, or even multiple messages each one carrying a payload.
+///
+/// **General format**
+///
+/// The format of a message, which is the same for both server and client is
+/// as follows. The sender of the message writes these sections of the message
+/// on the stream (the TCP connection) sequentially form left to right:
+///
+///  `<tag: u16>[header: fixed size per tag][payload: variable]`
+///
+/// The tag is a two-byte little-endian word which indicates the message type.
+/// Each type of message (that we in this document refer to as `type`, e.g.
+/// `List` for the List message) has it's own header.
+/// The message might not have a payload, e.g. `Error` which only has a header
+/// or it might just be a sinletag with no header and payload, e.g. `Ping` or `Pwd`
+/// The size of the header is fixed per the tag, meaning any `File` message header
+/// has a fixed size of `@sizeOf(FileHeader)`.
+/// All numbers must be litte-endian.
+///
+/// **Sending payloads**
+///
+/// The convention for sending payload which has a variable size is to specify the
+/// length in the header of the message (look as `CdHeader`). If the payload
+/// consists of two variable-size data (like file path & file content) then both
+/// pathes must be mentioned in the header and the data.
+/// In case of having to send may variable-size elements (like an array of strings)
+/// the convention is that the sender should send messages sequentially each
+/// containing a single payload, and then sending the `end` message to indicate the
+/// end of elements
+pub const Message = struct {
+    header: Header,
+};
 
+const TagType = u16;
+
+/// All headers must be packed structs {} as it's standardized
+/// by the language that packed structs have a guaranteed layout.
+/// Some of these messages are only expected to be sent from the
+/// client and others are responses sent from the server.
+pub const Header = union(enum(TagType)) {
+    Read: ReadHeader,
+    File: FileHeader,
+    List: ListHeader,
+    Entry: EntryHeader,
+    /// indictaes that the last element has been sent
+    End: EmptyHeader,
+    Cd: CdHeader,
+    Pwd: EmptyHeader,
+    Path: PathHeader,
+    /// sent by the server as a success response.
+    Ok: EmptyHeader,
+    GetInfo: EmptyHeader,
+    /// parameter negotiation message
+    Info: InfoHeader,
+    Ping: EmptyHeader,
+    PingReply: EmptyHeader,
+    /// can be sent from the client to terminate the connection
+    Quit: EmptyHeader,
+    QuitReply: EmptyHeader,
+    /// This is only returned by the message parser
+    /// to indicate that the peer has returned a
+    /// message with an unknown type tag. This message
+    /// type should not be sent by neither the client
+    /// nor the server.
+    Corrupt: CorruptHeader,
+    Error: ErrorHeader,
+};
+pub const EmptyHeader = packed struct {};
+pub const ReadHeader = packed struct {
+    length: u16,
+};
+pub const ListHeader = packed struct {
+    length: u16,
+};
+pub const CdHeader = packed struct {
+    /// length of the path payload
+    length: u16,
+};
+pub const FileHeader = packed struct {
+    size: u64,
+};
+pub const EntryHeader = packed struct {
+    length: u8,
+    is_dir: bool,
+};
+pub const PathHeader = packed struct {
+    length: u16,
+};
+pub const CorruptHeader = packed struct {
+    tag: TagType,
+};
+pub const ErrorHeader = packed struct {
+    code: u16,
+    arg1: u32,
+    arg2: u32,
+};
+pub const InfoHeader = struct {
+    max_name: usize,
+    max_path: usize,
+};
+
+/// These errors are returned by the server to indicate
+/// an error on the side of client.
 pub const ServerError = error{
     UnexpectedMessage,
     CorruptMessageTag,
@@ -10,7 +120,8 @@ pub const ServerError = error{
     IsNotDir,
     AccessDenied,
     CantOpen,
-
+    /// Just to indicate failure to decode error.
+    /// must never be returned from the server.
     Unrecognized,
 };
 
@@ -44,66 +155,8 @@ pub fn decodeServerError(code: u16) ServerError {
     };
 }
 
-const TagType = u16;
-
-pub const ReadHeader = packed struct {
-    length: u16,
-};
-pub const ListHeader = packed struct {
-    length: u16,
-};
-pub const CdHeader = packed struct {
-    length: u16,
-};
-pub const FileHeader = packed struct {
-    size: u64,
-};
-pub const EntryHeader = packed struct {
-    length: u8,
-    is_dir: bool,
-};
-pub const PathHeader = packed struct {
-    length: u16,
-};
-pub const CorruptHeader = packed struct {
-    tag: TagType,
-};
-pub const ErrorHeader = packed struct {
-    code: u16,
-    arg1: u32,
-    arg2: u32,
-};
-pub const EmptyHeader = packed struct {};
-
-pub const Header = union(enum(TagType)) {
-    Read: ReadHeader,
-    File: FileHeader,
-    List: ListHeader,
-    Entry: EntryHeader,
-    End: EmptyHeader,
-    Cd: CdHeader,
-    Pwd: EmptyHeader,
-    Path: PathHeader,
-    Ok: EmptyHeader,
-    GetInfo: EmptyHeader,
-    Info: InfoHeader,
-    Ping: EmptyHeader,
-    PingReply: EmptyHeader,
-    Quit: EmptyHeader,
-    QuitReply: EmptyHeader,
-    Corrupt: CorruptHeader,
-    Error: ErrorHeader,
-};
-
-pub const InfoHeader = struct {
-    max_name: usize,
-    max_path: usize,
-};
-
-pub const Message = struct {
-    header: Header,
-};
-
+/// `hdr` must be a pointer to a struct instance.
+/// Converts every integer field to little-endian.
 fn headerToLittle(hdr: anytype) void {
     var data = hdr;
     switch (@typeInfo(@TypeOf(data))) {
